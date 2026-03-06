f631cf073f4900b353d87ab5b5e2411e82e7cd3a
FerryBastian <sgnferry@gmail.com>
2026-03-06 09:17:05 +0700

    fix database

--- backend/database/migrations/2026_03_06_021624_create_submissions_table.php
+++ backend/database/migrations/2026_03_06_021624_create_submissions_table.php
@@ -6,17 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
-        Schema::table('submissions', function (Blueprint $table) {
-            $table->string('lokasi')->nullable()->change();
+        Schema::create('submissions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
-        Schema::table('submissions', function (Blueprint $table) {
-            $table->string('lokasi')->nullable(false)->change();
-        });
+        Schema::dropIfExists('submissions');
     }
 };
